15dd60295bf25633e7a8584d795773f2a647de8a
DiDongDongDi <2572931653@qq.com>
2019-07-09 08:01:45 +0800

    宿舍管理框架

---
+++ src/DormitoryManage.java
@@ -0,0 +1,41 @@
+import java.util.*;
+
+public class DormitoryManage{
+	
+	public void start(){//进入宿舍管理系统
+		System.out.println("欢迎进入宿舍管理系统!");
+		while(true){
+			System.out.println("请选择:");
+			System.out.println("1. 学生管理");
+			System.out.println("2. 宿舍楼管理");
+			System.out.println("3. 调换宿舍管理");
+			System.out.println("4. 查询系统");
+			System.out.println("0. 退出系统");
+			
+			Scanner sc = new Scanner(System.in);
+			int op = sc.nextInt();
+			switch(op){
+				case 1:
+					
+					break;
+				case 2:
+
+					break;
+
+				case 3:
+
+					break;
+				case 4:
+
+					break;
+				case 0:
+					System.exit(0);
+				default:
+					System.out.println("您的输入有误, 请重新输入!");
+			}
+		}
+		
+
+
+	}
+}
