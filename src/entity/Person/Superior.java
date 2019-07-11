3ab453affd92abb52d1674725d4209ca8b482df0
zhangzhaoxuan <1040517659@qq.com>
2019-07-11 09:29:26 +0800

    123

--- src/entity/Person/Superior.java
+++ src/entity/Person/Superior.java
@@ -19,23 +19,27 @@ public class Superior extends Person {
         Scanner in=new Scanner(System.in);
         int no;
         int sex;
+        boolean flg=true;
         System.out.println("欢迎办理入住手续！");
         System.out.print("请输入您的姓名：");
         String names=in.next();
         setName(names);
-        tt:System.out.print("请输入您的性别 1.男性 2.女性");
+        while(flg){
+            System.out.print("请输入您的性别 1.男性 2.女性");
         switch (in.nextInt())
         {
             case 1:
                 setSex(true);
+                flg=false;
                 break;
             case 2:
                 setSex(false);
+                flg=false;
                 break;
             default:
                 System.out.println("输入错误");
-                GOTO tt;
-        }
+                break;
+        }}
         System.out.print("请输入您的工号：");
         no=in.nextInt();
         setNo(no);
