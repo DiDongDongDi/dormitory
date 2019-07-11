42bb94c8e4948b78e61302d449bafb4cd1654394
DiDongDongDi <2572931653@qq.com>
2019-07-11 15:09:30 +0800

    开始宿舍楼部分

---
+++ src/Manage/BuildingManage/BuildingManage.java
@@ -0,0 +1,9 @@
+package Manage.BuildingManage;
+
+public class BuildingManage{
+	public void start(){
+		System.out.println("请选择:");
+		System.out.println("1. 新建宿舍楼");
+		System.out.println("2. ")
+	}
+}
