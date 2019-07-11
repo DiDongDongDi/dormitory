575a93b5973fc2694d2af109c5de5da6f76d6ab2
WT.Hao <43371826+DarcyWarden@users.noreply.github.com>
2019-07-11 10:51:38 +0800

    可以查询了

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
+import DataBase.DataBase;
+
 public class Main{
 	public static void main(String[] args){
+		DataBase.DataBase_Connect();
 		DormitoryManage DM = new DormitoryManage();
 		DM.start();
+		DataBase.DataBase_Disconnect();
 	}
 }
