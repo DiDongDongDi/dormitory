7c039a35780b0a0bb5e5e547e69e6afec137b14d
zhangzhaoxuan <33679893+zhangzhaoxuan@users.noreply.github.com>
2019-07-11 08:17:48 +0800

    更改goto

--- src/entity/Person/Student.java
+++ src/entity/Person/Student.java
@@ -27,22 +27,29 @@ public class Student extends Person implements implement{
         Scanner in=new Scanner(System.in);
         int no;
         int sex;
+        boolean flg;
         System.out.println("欢迎办理入住手续！");
         System.out.print("请输入您的姓名：");
         String names=in.next();
         setName(names);
-        tt:System.out.print("请输入您的性别 1.男性 2.女性");
+        while(flg){
+        System.out.print("请输入您的性别 1.男性 2.女性");
         switch (in.nextInt())
         {
             case 1:
                 setSex(true);
+                flg=false;
                 break;
             case 2:
                 setSex(false);
+                flg=false;
                 break;
+
             default:
                 System.out.println("输入错误");
-                GOTO tt;
+                flg=true;
+               break;
+        }
         }
         System.out.print("请输入您的学号：");
         no=in.nextInt();
