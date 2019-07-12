780f023b2c2b0649315f879905794422a63b5863
WT.Hao <43371826+DarcyWarden@users.noreply.github.com>
2019-07-12 10:57:33 +0800

    room实现了load,update,display

--- src/entity/building/room.java
+++ src/entity/building/room.java
@@ -1,5 +1,12 @@
 package entity.building;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Scanner;
+import DataBase.*;
+
+import java.util.Scanner;
 import Manage.RoomManage.RoomManage;
 
 public class room {
@@ -8,6 +15,24 @@ public class room {
     private int roomId;
     private int contain;
     private boolean []bedId;
+    private double elecBillBala;
+    private double healScore;
+
+    public void setElecBillBala(double elecBillBala) {
+        this.elecBillBala = elecBillBala;
+    }
+
+    public double getElecBillBala() {
+        return elecBillBala;
+    }
+
+    public void setHealScore(double healScore) {
+        this.healScore = healScore;
+    }
+
+    public double getHealScore() {
+        return healScore;
+    }
 
     public room()
     {contain=4;}
@@ -82,4 +107,106 @@ public class room {
         bedId[bedid]=true;
         System.out.println("入住成功！");
     }
+
+    private static boolean If_roomExists(int blockId,int floorId,int roomId){ //给定房间信息判断是否存在于数据库,静态,仅供操作数据库的方法内部使用
+        try{
+            PreparedStatement pstmt = null;
+            String sql="select * from rooms where buildId=? and floorId=? and roomId=?";//查找的sql
+            pstmt=DataBase.getConnection().prepareStatement(sql);
+            pstmt.setInt(1,blockId);
+            pstmt.setInt(2,floorId);
+            pstmt.setInt(3,roomId);
+            ResultSet rs=pstmt.executeQuery();
+
+            return rs.next();//是否找到room ?(boolean)
+
+        }catch (SQLException e) {
+            System.out.println("查找房间是否存在过程出现异常");//此处最后可以注释掉
+            //e.printStackTrace();                 //此处最后可以注释掉
+            return false;
+        }
+    }
+    public int display(int blockId,int floorId,int roomId){//给定房间查找并打印--hwt
+        //查找失败返回1
+        //未找到返回3
+        //直接从数据库查找显示即可
+        if(!If_roomExists(blockId,floorId,roomId)){//房间不存在
+            return 3;
+        }
+        try{
+            PreparedStatement pstmt = null;
+            String sql="select * from rooms where buildId=? and floorId=? and roomId=?";//查找的sql
+            pstmt=DataBase.getConnection().prepareStatement(sql);
+            pstmt.setInt(1,blockId);
+            pstmt.setInt(2,floorId);
+            pstmt.setInt(3,roomId);
+            ResultSet rs=pstmt.executeQuery();
+            while(rs.next()){//打印房间信息
+                System.out.println(
+                        rs.getInt(1)+
+                        rs.getInt(2)+
+                        rs.getInt(3)+
+                        rs.getDouble(4)+
+                        rs.getDouble(5));
+            }
+            return 0;//正常打印了学生信息
+        }catch (SQLException e) {//过程中出现异常
+            System.out.println("尝试显示学生信息:查找学生时出现异常");//此处最后可以注释掉
+            //e.printStackTrace();                 //此处最后可以注释掉
+            return 1;
+        }
+    }
+
+    public int load(){//已有主码的房间,从数据库获得其他信息
+
+        if(!If_roomExists(getBlockId(),getFloorId(),getRoomId())){//号码不存在!
+            return 2;
+        }
+        else{
+            try{
+                PreparedStatement pstmt = null;
+                String sql="select * from rooms where buildId=? and floorId=? and roomId=?";//查找的sql
+                pstmt=DataBase.getConnection().prepareStatement(sql);
+                pstmt.setInt(1,blockId);
+                pstmt.setInt(2,floorId);
+                pstmt.setInt(3,roomId);
+                ResultSet rs=pstmt.executeQuery();
+                rs.next();
+                setElecBillBala(rs.getDouble(4));
+                setHealScore(rs.getDouble(5));//
+
+
+                return 0;//load successfully
+            }catch (SQLException e) {//load room过程中出现异常
+                System.out.println("load房间时出现异常");//此处最后可以注释掉
+                e.printStackTrace();                 //此处最后可以注释掉
+                return 1;
+            }
+        }
+    }
+    public int update(){
+        if(!If_roomExists(getBlockId(),getFloorId(),getRoomId())){//号码不存在!
+            return 2;
+        }
+        else{
+            try{
+                PreparedStatement pstmt = null;
+                String sql="update rooms set elecBillBala=?,healScore=? where buildId=? and floorId=? and roomId=?";//查找的sql
+                pstmt=DataBase.getConnection().prepareStatement(sql);
+                pstmt.setDouble(1,getElecBillBala());
+                pstmt.setDouble(2,getHealScore());
+                pstmt.setInt(3,blockId);
+                pstmt.setInt(4,floorId);
+                pstmt.setInt(5,roomId);
+                pstmt.executeUpdate();
+
+                return 0;//update successfully
+            }catch (SQLException e) {//load room过程中出现异常
+                System.out.println("update房间时出现异常");//此处最后可以注释掉
+                e.printStackTrace();                 //此处最后可以注释掉
+                return 1;
+            }
+        }
+    }
+
 }
